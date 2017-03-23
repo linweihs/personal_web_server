5cd73b5d2c22ac1581e6f581b428dbf39c1d7c56
linweihs <maxlin@yahoo-inc.com>
2017-03-22 18:11:12 -0700

    configure express engine

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.use(app.router);
 
 // development only
