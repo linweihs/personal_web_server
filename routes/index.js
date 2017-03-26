e5a43f111365cc80b87a0e48b4a9e283824b5ab1
linweihs <aa10101491@gmail.com>
2017-03-26 13:08:12 -0700

    bootstrap ui

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  *   */
 
 exports.index = function(req, res){
-      res.render('index2', { title: 'Express' });
+      res.render('index3', { title: 'Express' });
 };
 
 /** serve jade enabled partials */
